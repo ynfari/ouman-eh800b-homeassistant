ac5c1af65226321ba53dca6d5beb9575aa84d39f
Jari Väisänen <ynfari@hotmail.com>
2025-09-25 22:51:36 +0300

    Update OumanHA.py

--- RaspberryPi-server/opt/OumanHA/OumanHA.py
+++ RaspberryPi-server/opt/OumanHA/OumanHA.py
@@ -1,3 +1,12 @@
+# Copyright (C) 2025 by Jari Väisänen
+# Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted.
+
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING 
+# ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. 
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR 
+# ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, 
+# NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
 from flask import Flask, jsonify
 import serial
 import time
@@ -18,7 +27,7 @@ measurement_names = [
 cached_measurements = {}
 
 def strip_units(value):
-    """Strips non-numeric characters from the measurement value."""
+    # Strips non-numeric characters from the measurement value.
     numeric_value = re.findall(r"[-+]?\d*\.?\d+", value)
     if numeric_value:
         return numeric_value[0]  # Return the first match as the numeric value
